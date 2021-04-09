d3a3e4e74b06417e56fc46d5cce58ef731cf723a
macbook <macbook@macbooks-MacBook-Pro-2.local>
2021-04-09 07:09:05 -0500

    styled popovers. shading applies to prices

--- app/src/components/calendar/calendar-day/CalendarDay.js
+++ app/src/components/calendar/calendar-day/CalendarDay.js
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
 import styles from './CalendarDay.module.scss';
 import CalendarDayPopover from './../calendar-day-popover/CalendarDayPopover';
-
-function shadeColor(color, percent) {
-
-    var R = parseInt(color.substring(1,3),16);
-    var G = parseInt(color.substring(3,5),16);
-    var B = parseInt(color.substring(5,7),16);
-
-    R = parseInt(R * (100 + percent) / 100);
-    G = parseInt(G * (100 + percent) / 100);
-    B = parseInt(B * (100 + percent) / 100);
-
-    R = (R<255)?R:255;
-    G = (G<255)?G:255;
-    B = (B<255)?B:255;
-
-    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-
-    return "#"+RR+GG+BB;
-}
+import Utils from './../../../utils/utils';
 
 const CalendarDay = ({ day }) => {
   const [hover, setHover] = useState(false)
   return (
     <div className={styles.calendarDay}>
       <div onMouseEnter={(e) => setHover(true)} onMouseLeave={(e) => setHover(false)}>
-        <div style={{ color: shadeColor('#53babf', -day.price * .08)}} className={styles.price}>
+        <div style={{ color: Utils.darkenColor('#6fc5ca', (day.price / 10))}} className={styles.price}>
           ${day.price}
         </div>
         {hover && <CalendarDayPopover day={day} />}
--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -10,7 +10,21 @@ const basePriceToListingDayPrice = (basePrice, factors) => {
     .toFixed(0);
 }
 
+// TODO: Adjust this function so UI representations of color differences are clearer
+// For now, just POC used to render cheaper dates a bit lighter/expensive dates darker
+const darkenColor = (color, percent) => {
+    var r=parseInt(color.substr(1,2),16);
+    var g=parseInt(color.substr(3,2),16);
+    var b=parseInt(color.substr(5,2),16);
+
+    return '#'+
+       Math.min(255,Math.floor(r*(1 - percent / 100))).toString(16)+
+       Math.min(255,Math.floor(g*(1 - percent / 100))).toString(16)+
+       Math.min(255,Math.floor(b*(1 - percent / 100))).toString(16);
+}
+
 export default {
   decimalToPercentageValue,
-  basePriceToListingDayPrice
+  basePriceToListingDayPrice,
+  darkenColor
 };
